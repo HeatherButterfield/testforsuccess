3bb5eb2b43ae3cce34757004230b52039095ee08
HeatherHyer <heather.hyer@familysearch.org>
2019-09-30 13:32:13 -0600

    buttons on home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,8 +6,18 @@ import BarGraph from "../assets/barGraph.png"
 function HomePage() {
   return (
     <div className="container">
-      <div className="row margin-bottom margin-top">
-        <img src={Header} alt="welcome" style={{ width: "100%" }}/>
+      <div class="row margin-top margin-bottom">
+        <div style={{backgroundImage: "url(" + Header + ")", backgroundSize: "cover", height: "800px"}}>
+          <div class="text-center py-5 px-4 margin-sides">
+            <div>
+              <h2 class="h1-responsive pt-3 mb-5 font-bold margin-top"><strong>Welcome to Test for Success!</strong></h2>
+              <p class="mx-5 mb-5">We are a testing software designed for elementary school teachers. Dicover how easy it is to create tests and then quickly test a student’s reading ability (letters, letter sounds, simple sight words, vowel families, etc) one on one. Student test data is at your fingertips. You can view and download student scores so you can target students for interventions.
+              </p>
+              <a class="btn text-dark yellow accent-1 btn-rounded btn-md">Sign-up</a>
+              <a href="/login" class="btn text-white rgba-cyan-strong btn-rounded btn-md">Login</a>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="row margin-bottom align-items-center">
         <div class="col-sm-12 col-md-6">
