0e6abe9a5aac6e2f57e613fd01ae12391258bce4
HeatherHyer <heather.hyer@familysearch.org>
2019-10-01 18:13:29 -0600

    responsive

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -21,11 +21,8 @@ function HomePage() {
         </div>
       </div>
       <div class="mobile">
-        <div class="row">
-          <img class="margin-none" src={MobileHeader} alt="teacher with students" style={{width: "100%"}}/>
-        </div>
         <div class="row text-center">
-          <div class="margin-bottom">
+          <div class="margin-bottom margin-top">
             <h2 class="h1-responsive font-bold"><strong>Welcome to Test for Success!</strong></h2>
             <p class="margin-sides">We are a testing software designed for elementary school teachers. Dicover how easy it is to create tests and then quickly test a student’s reading ability (letters, letter sounds, simple sight words, vowel families, etc) one on one. Student test data is at your fingertips. You can view and download student scores so you can target students for interventions.
             </p>
@@ -34,7 +31,7 @@ function HomePage() {
           </div>
         </div>
       </div>
-      <div className="row text-center margin-bottom align-items-center desktop">
+      <div className="row text-center margin-bottom align-items-center">
         <div class="col-sm-12 col-md-6">
           <img src={OneOnOne} alt="one on one" style={{ width: "90%" }}/>
         </div>
@@ -42,28 +39,12 @@ function HomePage() {
           <h2>Test students one on one</h2>
         </div>
       </div>
-      <div className="row text-center margin-bottom align-items-center desktop">
+      <div className="row text-center margin-bottom align-items-center">
         <div class="col-sm-12 col-md-6">
           <h2>View student test data</h2>
         </div>
         <div class="col-sm-12 col-md-6">
           <img src={BarGraph} alt="student data" style={{ width: "90%" }}/>
-        </div>
-      </div>
-      <div className="row text-center margin-bottom d-flex justify-content-conter align-items-center mobile">
-        <div class="col-sm-12 col-md-6">
-          <img src={OneOnOne} alt="one on one" style={{ width: "70%" }}/>
-        </div>
-        <div class="col-sm-12 col-md-6">
-          <h4>Test students one on one</h4>
-        </div>
-      </div>
-      <div className="row text-center margin-bottom d-flex justify-content-conter align-items-center mobile">
-        <div class="col-sm-12 col-md-6">
-          <img src={BarGraph} alt="student data" style={{ width: "70%" }}/>
-        </div>
-        <div class="col-sm-12 col-md-6">
-          <h4>View student test data</h4>
         </div>
       </div>
     </div>
