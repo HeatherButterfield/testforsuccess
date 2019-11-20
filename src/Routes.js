1d8ed0d1ff0b6f4a96531376f8ae3f10df525744
Ammon Packer <ammonpacker@ammons-mbp.ad.uvu.edu>
2019-11-20 16:54:22 -0700

    adding routes

--- src/Routes.js
+++ src/Routes.js
@@ -8,6 +8,7 @@ import StudentLoginPage from "./pages/StudentLoginPage";
 import TeacherLoginPage from "./pages/TeacherLoginPage";
 import StudentWaitingPage from "./pages/StudentWaitingPage";
 import TeacherDashboardPage from "./pages/TeacherDashboardPage"
+import TestsPage from "./pages/TestsPage";
 
 class Routes extends React.Component {
   render() {
@@ -20,6 +21,7 @@ class Routes extends React.Component {
         <Route exact path="/login/student" component={StudentLoginPage} />
         <Route exact path="/student/waiting" component={StudentWaitingPage} />
         <Route exact path="/teacher/dashboard" component={TeacherDashboardPage} />
+        <Route exact path="/teacher/tests" component={TeacherDashboardPage} />
 
         <Route
           render={function () {
