3c6640244f7130736a2dfa0b564cb418c0b0b0ad
Ammon Packer <ammonpacker@ammons-mbp.lan>
2019-11-19 23:57:43 -0700

    Added form content for all three cards

--- src/pages/TestCreationPage.js
+++ src/pages/TestCreationPage.js
@@ -5,74 +5,167 @@ function TestCreationPage() {
   return (
 
       <div class="container-fluid">
-        <div class="d-flex bd-highlight text-white">
-          <div class="p-2 flex-fill bd-highlight bg-warning">
+          <div class="row d-flex bd-highlight text-white align-items">
+            <div id="YellowContainer" class="col-lg-4 p-2 bd-highlight bg-warning">
+              <div class="border-0 card">
 
-            <div class="border-0 card">
+                <div class="card-body bg-warning">
 
+                  <i class="d-flex fas fa-edit fa-5x justify-content-center pb-5"></i>
+                  <form>
 
-              <div class="card-body bg-warning">
-
-                <i class="d-flex fas fa-edit fa-5x justify-content-center"></i>
-                <p class="card-text"></p>
-                <a href="#" class="d-flex btn btn-primary align-items-stretch justify-content-center"><h5 class="m-0">Edit Class</h5></a>
-
-              </div>
-
-            </div>
-
-          </div>
-
-          <div class="p-2 flex-fill bd-highlight bg-success">
-            <div class="border-0 card">
-
-              <div class="card-body bg-success">
-                <i class="d-flex fas fa-chart-bar fa-5x justify-content-center"></i>
-                <p class="card-text"></p>
-                <a href="#" class="d-flex btn btn-primary align-items-stretch justify-content-center"><h5 class="m-0">Edit Class</h5></a>
-              </div>
-
-            </div>
-          </div>
-
-          <div class="p-2 flex-fill bd-highlight bg-danger">
-            <div class="border-0 card">
-              <div class="card-body bg-danger">
-
-                <i class="d-flex fas fa-cogs fa-5x justify-content-center pb-3"></i>
-                <p class="card-text"></p>
-                <form>
-                  <div class="form-group row">
-                    <label for="classSelection" class="col-sm-3 col-form-label"><h5>Class</h5></label>
-                    <div class="col-sm-8">
-                      <select class="form-control" id="classSelection">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                      </select>
+                    <div class="form-group row mr-0 ml-0">
+                      <div class="col-sm-12">
+                        <select class="browser-default custom-select custom-select-lg ">
+                          <option selected>Select Class</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                        </select>
+                      </div>
                     </div>
-                  </div>
-                </form>
-                <a href="#" class="d-flex btn btn-primary align-items-stretch justify-content-center"><h5 class="m-0">Edit Class</h5></a>
 
-                  <label class="mdb-main-label">Blue select</label>
-                  <select class="mdb-select md-form colorful-select dropdown-danger">
-                    <option value="1">Option 1</option>
-                    <option value="2">Option 2</option>
-                    <option value="3">Option 3</option>
-                    <option value="4">Option 4</option>
-                    <option value="5">Option 5</option>
-                  </select>
-                <a href="#" class="d-flex btn btn-primary align-items-stretch justify-content-center"><h5 class="m-0">Modify Test</h5></a>
+                    <div class="form-group row mr-0 ml-0">
+                      <div class="col-sm-12">
+                        <select class="browser-default custom-select custom-select-lg ">
+                          <option selected>Select Student</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                        </select>
+                      </div>
+                    </div>
 
+                    <div class="form-group row mr-0 ml-0">
+                      <div class="col-sm-12">
+                        <select class="browser-default custom-select custom-select-lg ">
+                          <option selected>Select Test</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div class="form-group row">
+                      <div class="col-sm-12">
+                        <a href="#" class="d-flex align-self-end btn btn-primary justify-content-center"><h5 class="m-0">Test Student</h5></a>
+                      </div>
+                    </div>
+                  </form>
+                </div>
 
               </div>
 
             </div>
+
+            <div id="GreenContainer" class="col-lg-4 p-2 bd-highlight bg-success">
+              <div class="border-0 card">
+                <div class="card-body bg-success">
+
+                  <i class="d-flex fas fa-chart-bar fa-5x justify-content-center pb-5"></i>
+                  <form>
+                    <div class="form-group row mr-0 ml-0">
+                      <div class="col-sm-12">
+                        <select class="browser-default custom-select custom-select-lg ">
+                          <option selected>Select Student</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div class="form-group row">
+                      <div class="col-sm-12">
+                        <a href="#" class="d-flex btn btn-primary justify-content-center"><h5 class="m-0">View Student Data</h5></a>
+                      </div>
+                    </div>
+
+                    <div class="form-group row mr-0 ml-0">
+                      <div class="d-flex col-sm-12 justify-content-center text-dark">
+                        <h4>OR</h4>
+                      </div>
+                    </div>
+
+                    <div class="form-group row mr-0 ml-0">
+                      <div class="col-sm-12">
+                        <select class="browser-default custom-select custom-select-lg ">
+                          <option selected>Select Class</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div class="form-group row">
+                      <div class="col-sm-12">
+                        <a href="#" class="d-flex btn btn-primary justify-content-center"><h5 class="m-0">View Class Data</h5></a>
+                      </div>
+                    </div>
+
+                  </form>
+
+                </div>
+
+              </div>
+            </div>
+
+            <div id="RedContainer" class="col-lg-4 p-2 bd-highlight bg-danger">
+              <div class="border-0 card">
+                <div class="card-body bg-danger">
+
+                  <i class="d-flex fas fa-cogs fa-5x justify-content-center pb-5"></i>
+                  <form>
+                    <div class="form-group row mr-0 ml-0">
+                      <div class="col-sm-12">
+                        <select class="browser-default custom-select custom-select-lg ">
+                          <option selected>Select Class</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div class="form-group row">
+                      <div class="col-sm-12">
+                        <a href="#" class="d-flex btn btn-primary justify-content-center"><h5 class="m-0">Edit Class</h5></a>
+                      </div>
+                    </div>
+
+                    <div class="form-group row mr-0 ml-0">
+                      <div class="d-flex col-sm-12 justify-content-center text-dark">
+                        <h4>OR</h4>
+                      </div>
+                    </div>
+
+                    <div class="form-group row mr-0 ml-0">
+                      <div class="col-sm-12">
+                        <select class="browser-default custom-select custom-select-lg ">
+                          <option selected>Select Test</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div class="form-group row">
+                      <div class="d-flex col-sm-12 flex-row">
+                        <a href="#" class="btn btn-primary flex-fill"><h5 class="m-0">Modify Test</h5></a>
+                      </div>
+                    </div>
+
+                  </form>
+
+                </div>
+
+              </div>
+            </div>
           </div>
-        </div>
+
       </div>
 
   )
